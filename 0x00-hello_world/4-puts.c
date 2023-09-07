e331ea11e49fb3ef9fdc82fe51eb32fe2ab43e01
ossama11 <jeuxossama67@gmail.com>
2023-09-07 05:10:28 -0700

    fix error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
+include <stdio.h>
+
+/**
+ * main - prints a strings
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
-
