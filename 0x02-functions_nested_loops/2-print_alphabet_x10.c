26ab9e0b989da094fea4c294727048997b2db30b
ossama11 <jeuxossama67@gmail.com>
2023-09-12 11:28:29 -0700

    fix error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@
  * function print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  *
  * */
-void print_alphabet_x10(void) //print_alphabet_x10
+void print_alphabet_x10(void)
 {
 	char c;
 	int i, j;
