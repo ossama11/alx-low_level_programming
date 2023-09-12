16a69a2b11c1b98a577bfc79e7179a9ecc96a828
ossama11 <jeuxossama67@gmail.com>
2023-09-12 11:19:37 -0700

    fix error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,19 @@
  *
  * Return: Always 0 (Success)
  */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i, j;
 
-void print_alphabet_x10(void) {
-    char c;
-    int i, j;
-
-    for (j = 0; j < 10; j++) {
-        for (i = 97; i <= 122; i++) {
-            c = (char)i;
-            _putchar(c);
-        }
-        _putchar('\n');
-    }
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			c = (char)i;
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
+
