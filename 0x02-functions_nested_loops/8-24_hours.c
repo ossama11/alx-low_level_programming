316f3ed11805239e1fb816325cd3644257f8fce6
ossama11 <jeuxossama67@gmail.com>
2023-09-12 14:30:08 -0700

    fix error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- * main
- * */
-void jack_bauer(void) {
-    int hour, minute;
+ * jack_bauer - Print every minute of the day.
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++) {
-        for (minute = 0; minute < 60; minute++) {
-            printf("%02d:%02d\n", hour, minute);
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);
+		}
+	}
 }
 
