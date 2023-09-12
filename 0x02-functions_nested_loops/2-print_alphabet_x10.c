786eb55987f6ce27d43cb17a3095f3f0a89943e2
ossama11 <jeuxossama67@gmail.com>
2023-09-12 09:05:44 -0700

    hello

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the alphabet in lowercase
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 void print_alphabet_x10(void) {
     char c;
     int i, j;
