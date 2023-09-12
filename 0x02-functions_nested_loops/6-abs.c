e52632b2c040ddefbcb485062265fe406e3c2401
ossama11 <jeuxossama67@gmail.com>
2023-09-12 14:20:07 -0700

    fix error

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- *main
+ * _abs - Compute the absolute value of an integer.
+ * @n: The integer to compute the absolute value of.
+ *
+ * Return: The absolute value of the integer.
  */
-int _abs(int n) {
-    if (n < 0) {
-        return (-n);
-    } else {
-        return (n);
-    }
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }
 
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,31 @@
 #include "main.h"
 #include <limits.h>
 
-int print_last_digit(int n) {
-    int last_digit;
+/**
+ * print_last_digit - Print the last digit of an integer.
+ * @n: The integer to extract the last digit from.
+ *
+ * Return: The last digit of the integer.
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
 
-    if (n == INT_MIN) {
-        last_digit = 8;
-    } else {
-        if (n < 0) {
-            n = -n;
-        }
+	if (n == INT_MIN)
+	{
+		last_digit = 8;
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = -n;
+		}
+		last_digit = n % 10;
+	}
 
-        last_digit = n % 10;
-    }
+	_putchar('0' + last_digit);
 
-    _putchar('0' + last_digit);
-
-    return last_digit;
+	return (last_digit);
 }
 
