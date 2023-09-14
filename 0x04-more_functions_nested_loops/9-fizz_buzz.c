06f03aaa4a404919ff8d2075e092623e3ca0b66d
ossama11 <jeuxossama67@gmail.com>
2023-09-14 10:07:00 -0700

    fix the fu**** betty style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 
-/*
- * main - hello
+/**
+ * main - Entry point of the program
  *
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
-    for (i = 0; i <= 100; i++)
-    {
-        if (i % 3 == 0)
-        {
-            printf("Fizz ");
-            if (i % 5 == 0)
-            {
-                printf("Buzz ");
-            }
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
-    return (0);
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+	return (0);
 }
 
