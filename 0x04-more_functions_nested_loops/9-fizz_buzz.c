f66a69a6928238723fbfb21c4683a0560da0bdd2
ossama11 <jeuxossama67@gmail.com>
2023-09-14 10:31:16 -0700

    fix the fu**** betty style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,26 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
-			if (i % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-		}
-		else if (i % 5 == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
-		}
-		else
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("%d ", i);
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
 		}
 	}
+	printf("\n");
 
 	return (0);
 }
-
