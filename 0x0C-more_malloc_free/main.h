1dc5129b13cf08fad94682a051f4e6630e2307dd
ossama11 <jeuxossama67@gmail.com>
2023-10-08 10:09:14 +0100

    last commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
