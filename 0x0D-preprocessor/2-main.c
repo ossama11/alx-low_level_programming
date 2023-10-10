854c0fcb264b357a3f0defd8c0ebd8eb4d3b8449
ossama11 <jeuxossama67@gmail.com>
2023-10-10 12:01:35 +0100

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program.
+ * It prints the name of the current source file using the __FILE__ macro.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+    printf("%s\n", __FILE__);
+
+    return (0);
+}
