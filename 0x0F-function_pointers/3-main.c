630a1415eebe76e411932271cc090ee61418915f
ossama11 <jeuxossama67@gmail.com>
2023-10-11 12:47:28 +0100

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* main - performs simple operations
+* @argc: number of command-line arguments
+* @argv: array of command-line arguments
+*
+* Return: 0 upon success, otherwise 98, 99, or 100
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
