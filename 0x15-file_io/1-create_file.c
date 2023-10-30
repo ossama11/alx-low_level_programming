401944d79c05afa7549b6957eeadbacae59e26b6
ossama11 <jeuxossama67@gmail.com>
2023-10-30 12:19:06 +0100

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+* create_file - Creates a file and writes text content into it
+* @filename: The name of the file to create
+* @text_content: The content to write to the file
+*
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[bytes_written])
+			bytes_written++;
+
+		if (write(fd, text_content, bytes_written) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
