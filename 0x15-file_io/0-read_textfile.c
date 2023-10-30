19aacd6cb45589ef1e0400d62bfd0ada4616843d
ossama11 <jeuxossama67@gmail.com>
2023-10-30 12:10:48 +0100

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,56 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "main.h"
 
 /**
 * read_textfile - Reads a text file and prints it to the POSIX standard output
 * @filename: The name of the file to read
 * @letters: The number of letters to read and print
 *
-* Return: The actual number of letters read and printed
+* Return: The actual number of letters read and printed, 0 otherwise
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
+		return (0);
+
 	if (filename == NULL)
-		return (0);
-
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
-	char buffer[letters];
-
-	ssize_t bytes_read = read(fd, buffer, letters);
-
-	if (bytes_read == -1)
 	{
-		close(fd);
+		free(text);
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		close(fd);
+		free(text);
 		return (0);
 	}
 
-	close(fd);
-	return (bytes_written);
+	let = read(file, text, letters);
+	if (let == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, text, let);
+	if (w == -1 || w != let)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+
+	free(text);
+	close(file);
+
+	return (w);
 }
